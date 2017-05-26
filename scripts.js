f63ac09a5d803bb310fd57c643cecea0ac6e3692
Filip Wielanier <filipwielanier@gmail.com>
2017-05-26 22:54:14 +0200

    treeUpdate-26-05

--- scripts.js
+++ scripts.js
@@ -1,3 +1,32 @@
-for ( var k = 0 ; k < 10 ; k++) {
-  console.log('The value of k is now: ' + k);
+function drawTree(rows) {
+  var star;
+  for (var i = 0; i <= rows; i++){
+    var star = '';
+    for (var j = 0; j < i; j++){
+      star += '*';
+      // star = star + '*';
+    }
+    console.log(star)
+  }
 }
+
+//drawTree(16)
+
+function drawStraightTree(rows) {
+  var star = '';
+  for (var i = 0; i <= rows; i++){
+    star = '';
+    // spacje
+    for (var j = 0; j < rows - i; j++){
+      star += ' ';
+    }
+    // gwiazdki
+    for (var k = 0; k < 2 * i - 1; k++){
+      star += '*';
+    }
+    console.log(star);
+  }
+}
+
+
+drawStraightTree(13)
